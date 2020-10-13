be92c3be165f7764e62e2c4cd20a4f104d7c1ac6
Alexander Wood <alexljwood24@hotmail.co.uk>
2020-10-13 11:34:32 +0100

    Start to move away from Guice... it doesn't work :)

--- src/main/kotlin/me/bristermitten/ppm/PPM.kt
+++ src/main/kotlin/me/bristermitten/ppm/PPM.kt
@@ -4,8 +4,11 @@ import co.aikar.commands.PaperCommandManager
 import com.google.inject.Guice
 import dev.misfitlabs.kotlinguice4.getInstance
 import me.bristermitten.ppm.command.PPMCommand
+import me.bristermitten.ppm.entity.meta.PluginYMLMetadataExtractor
 import me.bristermitten.ppm.inject.PPMModule
 import me.bristermitten.ppm.inject.RepositoryFactory
+import me.bristermitten.ppm.service.DefaultPackageDownloader
+import me.bristermitten.ppm.service.PluginPackageManager
 import org.bukkit.plugin.java.JavaPlugin
 
 class PPM : JavaPlugin()
@@ -17,13 +20,12 @@ class PPM : JavaPlugin()
         val repo = RepositoryFactory().createSpigetRepo()
         val injector = Guice.createInjector(PPMModule(this, repo, pluginsDirectory))
 
-        val manager = PaperCommandManager(this)
-        manager.registerCommand(injector.getInstance<PPMCommand>())
+//        val metadataExtractor = PluginYMLMetadataExtractor()
+        val downloader = DefaultPackageDownloader(repo ,pluginsDirectory )
+//        val packageManager = PluginPackageManager(downloader)
+        val manager = PaperCommandManager(this)S
+        val command = PPMCommand(repo, packageManager)
+        manager.registerCommand(command)
 
-    }
-
-    override fun onDisable()
-    {
-        // Plugin shutdown logic
     }
 }
